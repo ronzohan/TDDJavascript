8f79ae8c1957a816dca0db4b6a9f95c012e1b12c
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-30 14:36:15 +0800

    Added initail files for JavascriptTDD

---
+++ 1/src/strftime.js
@@ -0,0 +1,101 @@
+Date.prototype.strftime = (function(){
+    function strftime(format){
+    	var date = this;
+
+    	return (format + "").replace(/%([a-zA-Z])/g, 
+    	function (m, f){
+    		var formatter = Date.formats && Date.formats[f];
+
+    		if (typeof formatter == "function")
+    		{
+    			return formatter.call(Date.formats, date);
+    		} 
+    		else if (typeof formatter == "string")
+    		{
+    			return date.strftime(formatter);
+    		}
+
+    		return f;
+    	});
+	}
+
+//Internal Helper
+function zeroPad(num)
+{
+	return (+num < 10 ? "0" : "") + num;
+}
+
+Date.formats = {
+	//Formatting methods 
+	d: function(date){
+		return zeroPad(date.getDate());
+	},
+
+	m: function(date){
+		return zeroPad(date.getMonth());
+	},
+
+	y: function(date){
+		return zeroPad(date.getYear() % 100);
+	},
+
+	Y: function(date){
+		return date.getFullYear();
+	},
+
+	// Format shorthands
+	F: "%Y-%m-%d",
+	D: "%m/%d/%y"
+};
+
+return strftime;
+}());
+
+function assert(message, expr){
+	if (!expr){
+		throw new Error(message)
+		
+	}
+	
+	assert.count++;
+	
+	return true
+}
+
+assert.count = 0;
+
+
+function output(text, color){
+	var p = document.createElement("p");
+	p.innerHTML = text;
+	p.style.color = color;
+	document.body.appendChild(p);
+}
+
+
+function testCase(name, tests){
+	assert.count = 0;
+	var successful = 0;
+	var testCount = 0;
+	
+	for (var test in tests){
+		if (!/^test/.test(test)){
+			continue;
+		}
+		
+		testCount++;
+		
+		try{
+			tests[test]();
+			output(test, "#0c0");
+			successful++;
+		} catch(e){
+			output(test + " failed: " + e.message, "#c00");
+		}
+	}
+	
+	var color = successful == testCount ? "#0c0" : "#c00";
+	
+	output("<strong>" + testCount + " tests, " + 
+		  (testCount - successful) + " failures</strong>", color)
+}
---
+++ 1/src/strftime_test.js
@@ -0,0 +1,24 @@
+var date = new Date(2009, 9, 5);
+testCase("strftime test", {
+	"test format specifier %Y": function(){
+		assert("%Y should return full year",
+				date.strftime("%Y") === "2009");
+	},
+	
+	"test format specifier %m": function(){
+		assert("%m should return month",
+				date.strftime("%m") === "09");
+	},
+	"test format specifier %d": function () {
+		assert("%d should return date",
+				date.strftime("%d") === "05");
+	},
+	"test format specifier %y": function () {
+		assert("%y should return year as two digits",
+				date.strftime("%y") === "09");
+	},
+	"test format shorthand %F": function () {
+		assert("%F should act as %Y-%m-%d",
+				date.strftime("%F") === "2009-09-05");
+	}
+});
